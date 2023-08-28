cf906e474c75a0389ccb7ef4383ea04490efc7d3
agni7601083 <agni7601083@gmail.com>
2023-08-28 14:23:34 +0530

    first features of book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { AddBookComponent } from './book/add-book/add-book.component';
+import { ViewBookComponent } from './book/view-book/view-book.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    AddBookComponent,
+    ViewBookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/book/add-book/add-book.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-add-book',
+  templateUrl: './add-book.component.html',
+  styleUrls: ['./add-book.component.css']
+})
+export class AddBookComponent {
+
+  constructor(private http:HttpClient) {}
+
+  url:string='http://localhost:3000/student';
+
+  addBook(addBookForm: any): void {
+    const formData = addBookForm.value;
+
+    this.http.post(this.url, formData).subscribe(
+      (response: any) => {
+        // console.log('Server Response:', response);
+        // console.log('Student Added Successfully');
+        alert('Student Added Successfully');
+      },
+      (error: any) => {
+        console.error('Error:', error);
+      }
+    );
+  }
+
+}
